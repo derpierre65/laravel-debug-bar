dfa51486eb40268e7ea62e79e9b7df00e995c49b
derpierre65 <git@derpierre65.de>
2021-05-18 20:47:42 +0200

    add test debug bar component

---
+++ resources/js/DebugBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="position:absolute;bottom:0;height:20px;background:red;">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DebugBar',
+}
+</script>
