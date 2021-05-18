bdbb99a9c104145e446e0dd9a2520b49fc3933c9
derpierre65 <git@derpierre65.de>
2021-05-18 20:39:36 +0200

    added format filesize method

--- src/Http/Middleware/DebugMiddleware.php
+++ src/Http/Middleware/DebugMiddleware.php
@@ -8,8 +8,14 @@ use Illuminate\Support\Str;
 
 class DebugMiddleware
 {
-    public function handle(Request $request, \Closure $next) {
+    public function handle(Request $request, \Closure $next)
+    {
         $response = $next($request);
+
+        // fallback if LARAVEL_START is undefined
+        if (!defined('LARAVEL_START')) {
+            define('LARAVEL_START', microtime(true));
+        }
 
         $queries = DB::connection()->getQueryLog();
         $executionTime = microtime(true) - LARAVEL_START;
@@ -20,9 +26,9 @@ class DebugMiddleware
             'update' => 0,
             'delete' => 0,
         ];
-        foreach ( $queries as $query ) {
-            foreach ( $queryTypes as $type => &$count ) {
-                if ( Str::startsWith(strtolower($query['query']), $type) ) {
+        foreach ($queries as $query) {
+            foreach ($queryTypes as $type => &$count) {
+                if (Str::startsWith(strtolower($query['query']), $type)) {
                     $count++;
                     break;
                 }
@@ -34,12 +40,33 @@ class DebugMiddleware
         $response->headers->add([
             'X-DEBUG-QUERIES' => json_encode($queryTypes),
             'X-DEBUG-QUERY-LOG' => json_encode($queries),
-            'X-DEBUG-MEMORY' => format_filesize(memory_get_peak_usage(true)),
-            'X-DEBUG-EXECUTION-TIME' => round($executionTime, 2).' (with debug: '.round(microtime(true) - LARAVEL_START, 2).')',
+            'X-DEBUG-MEMORY' => $this->formatFilesize(memory_get_peak_usage(true)),
+            'X-DEBUG-EXECUTION-TIME' => round($executionTime, 2) . ' (with debug: ' . round(microtime(true) - LARAVEL_START, 2) . ')',
         ]);
 
         return $response;
+    }
 
-        return $response;
+    private function formatFilesize(int $byte, int $precision = 2): string
+    {
+        $symbol = 'Byte';
+        if ($byte >= 1000) {
+            $byte /= 1000;
+            $symbol = 'kB';
+        }
+        if ($byte >= 1000) {
+            $byte /= 1000;
+            $symbol = 'MB';
+        }
+        if ($byte >= 1000) {
+            $byte /= 1000;
+            $symbol = 'GB';
+        }
+        if ($byte >= 1000) {
+            $byte /= 1000;
+            $symbol = 'TB';
+        }
+
+        return round($byte, $precision) . ' ' . $symbol;
     }
 }
